50d23909f13ce47b1ccd35353e3e15e54df58de1
Dillon Long <dplong@crimson.ua.edu>
2020-08-11 16:36:40 -0700

    Added a route for home page to pages.js and made it redirect to login page if user is not signed in.

--- routes/pages.js
+++ routes/pages.js
@@ -3,16 +3,24 @@ const router = express.Router();
 
 const csrfProtection = require('csurf')({cookie: true});
 
+router.get('/', (req, res) => {
+  if(!req.user) res.redirect('/login');
+  res.send('Home Page!');
+});
+
+//Login page route
 router.get('/login', csrfProtection, (req, res) => {
   if(req.user) res.redirect('/');
   res.render('login', { csrf: req.csrfToken()});
 });
 
+//Signup page route
 router.get('/signup', csrfProtection, (req, res) => {
   if(req.user) res.redirect('/');
   res.render('signup', { csrf: req.csrfToken()});
 });
 
+//Catch all for routes we have not defined
 router.get('*', (req, res) => {
   res.render('error-page');
 })

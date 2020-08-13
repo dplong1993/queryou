7deb200c9b24adc5352d7267b467e6c2a96cd733
Dillon Long <dplong@crimson.ua.edu>
2020-08-13 13:55:31 -0700

    Added more seed data for users and questions

--- db/seeders/20200810194232-DemoUser.js
+++ db/seeders/20200810194232-DemoUser.js
@@ -4,9 +4,11 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      username: 'DemoUser', firstName: "Demo", lastName: "User", email: 'demo@user.io', hashedPassword: await bcrypt.hash('password', 10), description: 'DEMO DESCRIPTION'
-    }], { fields: ['username', 'firstName', 'lastName', 'email', 'hashedPassword']});
+    await queryInterface.bulkInsert('Users', [
+      {username: 'DemoUser', firstName: "Demo", lastName: "User", email: 'demo@user.io', hashedPassword: await bcrypt.hash('password', 10), description: 'DEMO DESCRIPTION'},
+      {username: 'DemoUser1', firstName: "Demo1", lastName: "User", email: 'demo1@user.io', hashedPassword: await bcrypt.hash('password', 10), description: 'DEMO DESCRIPTION'},
+      {username: 'DemoUser2', firstName: "Demo2", lastName: "User", email: 'demo2@user.io', hashedPassword: await bcrypt.hash('password', 10), description: 'DEMO DESCRIPTION'}
+    ], { fields: ['username', 'firstName', 'lastName', 'email', 'hashedPassword']});
   },
 
   down: async (queryInterface, Sequelize) => {
--- db/seeders/20200813165952-DemoQuestions.js
+++ db/seeders/20200813165952-DemoQuestions.js
@@ -5,9 +5,11 @@ const {User} = db;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const demoUser = await User.findOne({where: {id: 1}});
+    const users = await User.findAll();
     await queryInterface.bulkInsert('Questions', [
-      {content: 'Is this a question?', ownerId: demoUser.id},
+      {content: 'Is this a question?', ownerId: users[0].id},
+      {content: 'What is the meaning of life?', ownerId: users[1].id},
+      {content: 'How long could CSS really take?', ownerId: users[2].id},
     ], { fields: ['content', 'ownerId']});
   },
 

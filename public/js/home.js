5906dfc571ed42cbe4f2b12b12eb680861a3a263
Shan <shanw3@uci.edu>
2020-08-14 15:56:26 -0400

    display profile frontend successful

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,38 @@
+//const answer = require("../../db/models/answer");
+
 const nav = document.querySelectorAll(".top-nav li");
 
 const getAnswers = async () => {
-    const res = await fetch('/api/')
+    const res = await fetch('/api/answers');
+    const data = await res.json();
+    return data;
+};
+
+const populateAnswersList = async () => {
+    const answersList = document.querySelector('.answers-list');
+    const { answers } = await getAnswers();
+    for (let answer of answers) {
+        const createdAt = new Date(answer.createdAt);
+        const dateOptions = {
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+        };
+        const timestamp = createdAt.toLocaleString("en-US", dateOptions);
+        const answerLi = `
+            <li>
+                <div class="user-icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="answer">
+                    <div class="answer-header">
+                        Updated ${answer.User.username} · ${timestamp}
+                    </div>                
+                </div>
+            </li>
+        `;
+        answersList.innerHTML += answerLi;
+    }
 }
+populateAnswersList();
+

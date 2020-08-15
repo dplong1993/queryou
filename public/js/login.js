b30342685bd381a48746d5b3cdb8dcc0720cfd8d
Daniel Wilkins <11danielw@gmail.com>
2020-08-15 11:12:09 -0400

    Added demo button css and backend attempt

--- public/js/login.js
+++ public/js/login.js
@@ -4,11 +4,45 @@ import { redirectIfLoggedIn } from './utils/auth.js';
 redirectIfLoggedIn();
 
 const form = document.querySelector('#login-form');
+const demoForm = document.querySelector("#demo-form");
+
+demoForm.addEventListener('submit', async (e) => {
+  console.log("submitting");
+  e.preventDefault();
+  const formData = new FormData(demoForm);
+  console.log(formData);
+  const email = formData.get('demo-email');
+  const password = formData.get('demo-password');
+  const _csrf = formData.get('_csrf');
+
+  const body = { email, password, _csrf };
+
+  const res = await fetch('/api/users/token', {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  const data = await res.json();
+  if(!res.ok){
+    const {message} = data;
+    const errorsContainer = document.querySelector('#errors-container');
+    errorsContainer.innerHTML = message;
+    const banner = document.getElementById("banner");
+    banner.classList.add("isVisible");
+    setTimeout(()=> banner.classList.remove("isVisible"),7000);
+    return
+  }
+
+  window.location.href = '/';
+});
 
 form.addEventListener('submit', async (e) => {
   console.log("submitting");
   e.preventDefault();
   const formData = new FormData(form);
+  console.log(formData);
   const email = formData.get('email');
   const password = formData.get('password');
   const _csrf = formData.get('_csrf');

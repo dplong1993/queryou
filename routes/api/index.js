985d3d998840243b424aab03f5011e39298b6284
Dillon Long <dplong@crimson.ua.edu>
2021-04-29 15:57:54 -0500

    Changed the routing so that the home page on the frontend corresponds to "/" rather than "/home"

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userRouter = require('./users');
-const queriesRouter = require('./queries');
-const interestsRouter = require('./interests');
-const topicsRouter = require('./topics');
-const questionTopicsRouter = require('./questionTopics');
+const userRouter = require("./users");
+const queriesRouter = require("./queries");
+const interestsRouter = require("./interests");
+const topicsRouter = require("./topics");
+const questionTopicsRouter = require("./questionTopics");
 const questionsRouter = require("./question");
 const homeRouter = require("./home");
 
-const { environment } = require('../../config');
-const { ValidationError } = require('sequelize');
-const { getUserFromToken } = require('../utils/auth');
+const { environment } = require("../../config");
+const { ValidationError } = require("sequelize");
+const { getUserFromToken } = require("../utils/auth");
 
 //Sets up key of user on all reqs if there is a token in the cookies
 // and sets the value of the key to the user the token corresponds to.
@@ -25,25 +25,25 @@ router.use(async (req, res, next) => {
   const user = await getUserFromToken(token);
 
   //If getting user was successful, set key of user on req to the fetched user
-  if(user) req.user = user;
+  if (user) req.user = user;
 
   next();
 });
 
-router.use('/home', homeRouter);
-router.use('/users', userRouter);
-router.use('/queries', queriesRouter);
-router.use('/interests', interestsRouter);
-router.use('/topics', topicsRouter);
-router.use('/questionTopics', questionTopicsRouter);
-router.use('/questions', questionsRouter);
+router.use("/home", homeRouter);
+router.use("/users", userRouter);
+router.use("/queries", queriesRouter);
+router.use("/interests", interestsRouter);
+router.use("/topics", topicsRouter);
+router.use("/questionTopics", questionTopicsRouter);
+router.use("/questions", questionsRouter);
 
 router.use((err, req, res, next) => {
-  if(err instanceof ValidationError){
-    err.errors = err.errors.map(e => e.message);
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
   }
   next(err);
-})
+});
 
 router.use((err, req, res, next) => {
   res.status(err.status || 500);
